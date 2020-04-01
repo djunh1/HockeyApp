021b9118e06df3265b0c191cf47d7b8e61b8308d
djunh1 <djunh1@gmail.com>
2020-04-01 16:56:22 -0400

    Add Styles, third party libraries

--- HockeyApp-SPA/src/app/_services/auth.service.ts
+++ HockeyApp-SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 // Decerator.  Can inject anything into the service.
 // Components injecatble by default, services are not.
@@ -9,24 +10,33 @@ import { map } from 'rxjs/operators';
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-login(model: any){
-  return this.http.post(this.baseUrl + 'login', model)
-    .pipe(
-      map((response: any) => {
-        const user = response;
-        if(user){
-          localStorage.setItem('token', user.token);
-        }
-      })
-    );
-}
+  login(model: any){
+    return this.http.post(this.baseUrl + 'login', model)
+      .pipe(
+        map((response: any) => {
+          const user = response;
+          if (user){
+            localStorage.setItem('token', user.token);
+            this.decodedToken = this.jwtHelper.decodeToken(user.token);
+            console.log(this.decodedToken);
+          }
+       })
+      );
+  }
 
-register(model: any){
-  // Must subscribe to the observable in the component which uses this.
-  return this.http.post(this.baseUrl + 'register', model);
-}
+  register(model: any){
+    // Must subscribe to the observable in the component which uses this.
+    return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
 
 }
